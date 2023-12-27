ced027bbb00e2c63ab78f94596aa34473c52c8b6
jafy0234 <134864063+jafy0234@users.noreply.github.com>
2023-12-27 10:43:31 -0600

    Update 1003.java

--- INICIANTE/1003.java
+++ INICIANTE/1003.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
    public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int A= sc.nextInt()+sc.nextInt;
+    int A= sc.nextInt()+sc.nextInt();
     System.out.println("SOMA = "+A);
     }
 }
