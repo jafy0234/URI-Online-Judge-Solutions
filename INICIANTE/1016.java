ea36f8dc8e87217e4228b52e2d0eb3b64c8683b0
jafy0234 <134864063+jafy0234@users.noreply.github.com>
2023-12-27 10:54:44 -0600

    Create 1016.java

---
+++ INICIANTE/1016.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+        public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int X= sc.nextInt();
+    
+    System.out.printf("%d minutos\n",X*2);
+    }
+ 
+}
